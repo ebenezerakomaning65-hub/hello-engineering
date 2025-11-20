b1fcb4428bc1f758f2bbef85c3f5c1cc8f7fde45
ebenezerakomaning65-hub <ebenezerakomaning65@gmail.com>
2025-11-20 21:15:26 +0400

    third commit

---
+++ index.py
@@ -0,0 +1,6 @@
+ # This is my fisrt script for the introduction to engineering
+# Akomaning Ebenezer
+
+print("hello, Russia")
+print(f" I am a future engineer, and name is Akomaning Ebenezer")
+print("This project is now on Github!")
